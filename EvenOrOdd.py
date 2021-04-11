ba233b3b50bb4d250150cdb97e2ffdfe9a0a20d8
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-11 16:09:46 +0530

    EvenOdd modified

---
+++ EvenOrOdd.py
@@ -0,0 +1,14 @@
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
+
+
+print(even_or_odd(-15))
+
+
+# another variation in the code
+
+"""return 'odd' if number % 2 else 'even'"""
+
+# another variation in the code
+
+"""return ["even","odd"][number % 2]"""
