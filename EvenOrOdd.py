96321002965580e6f429440a94388915fb63e1ec
Rajat Sharma <rajatwithphy@gmail.com>
2021-04-11 19:31:07 +0530

    added comments

--- EvenOrOdd.py
+++ EvenOrOdd.py
@@ -6,6 +6,8 @@ print(even_or_odd(-15))
 
 
 # another variation in the code
+# number % 2 will return 0 for even numbers and 1 for odd ones.
+# 0 evaluates to False and 1 to True, so we can do it with one line
 
 """return 'odd' if number % 2 else 'even'"""
 
